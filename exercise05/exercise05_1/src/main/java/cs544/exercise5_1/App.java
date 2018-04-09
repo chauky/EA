2cf7d358278d113e82e50d05a1df6941dacad3a1
chauky <nguyenchauky@gmail.com>
2018-04-09 17:58:36 -0500

    Commit ex 5.1

--- exercise05/exercise05_1/src/main/java/cs544/exercise5_1/App.java
+++ exercise05/exercise05_1/src/main/java/cs544/exercise5_1/App.java
@@ -1,18 +1,29 @@
 package cs544.exercise5_1;
 
-public class App
-{
-	public static void main(String[] args) {
-		IProductService productService = new ProductService();
 
-		Product product1 = productService.getProduct(423);
-		if (product1 != null) {
-			System.out.println(product1.toString());
-		}
-		Product product2 = productService.getProduct(239);
-		if (product2 != null) {
-			System.out.println(product2.toString());
-		}
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	private static ApplicationContext context;
+	private static IProductService productService;
+
+	public static void main(String[] args) {
+		// IProductService productService = new ProductService();
+		//
+		// Product product1 = productService.getProduct(423);
+		// if (product1 != null) {
+		// System.out.println(product1.toString());
+		// }
+		// Product product2 = productService.getProduct(239);
+		// if (product2 != null) {
+		// System.out.println(product2.toString());
+		// }
+
+		context = new ClassPathXmlApplicationContext("springconfig.xml");
+		productService = context.getBean("productService", IProductService.class);
+		System.out.println(productService.getProduct(234));
+		System.out.println(productService.getProduct(239));
 
 	}
 }
